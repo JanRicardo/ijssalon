aaa886d7ab9d427cdd275acb1059c58bd36a0be1
JanRicardo <139908980+JanRicardo@users.noreply.github.com>
2025-09-05 18:20:29 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,2 @@
 print("hello wereld !")
-<<<<<<< HEAD
-print("welkom in mijn ijssalon")
-=======
 print("welkom in onze ijssalon")
-print("dit is een git pull")
->>>>>>> ff5236d6fe24da05528bdb31ad7124162a21eb52
